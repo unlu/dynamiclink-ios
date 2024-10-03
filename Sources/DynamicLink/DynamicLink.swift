9cb0a9d295a823f3661e03a6aade827b76941406
Mehmet Ünlü <mehmetunlu00@gmail.com>
2024-10-03 15:21:52 +0300

    initial commit

--- Sources/DynamicLink/DynamicLink.swift
+++ Sources/DynamicLink/DynamicLink.swift
@@ -1,2 +1,8 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+public struct DynamicLinkStruct {
+    public static func hello() -> String {
+        "say hello"
+    }
+}
