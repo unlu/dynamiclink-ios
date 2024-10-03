80cdd523a580d42cf81076c71c11d347695fc7e1
Mehmet Ünlü <mehmetunlu00@gmail.com>
2024-10-03 15:47:36 +0300

    binary added

--- Package.swift
+++ Package.swift
@@ -12,10 +12,10 @@ let package = Package(
             targets: ["DynamicLink"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "DynamicLink"),
-
-    ]
+            // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+            // Targets can depend on other targets in this package, and on products in packages this package depends on.
+            .binaryTarget(
+                name: "DynamicLink",
+                path: "./Sources/DynamicLink.xcframework")
+        ]
 )
---
+++ Sources/DynamicLink.xcframework/ios-arm64/DynamicLink.framework/Headers/DynamicLink.h
@@ -0,0 +1,18 @@
+//
+//  DynamicLink.h
+//  DynamicLink
+//
+//  Created by Personal on 3.10.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DynamicLink.
+FOUNDATION_EXPORT double DynamicLinkVersionNumber;
+
+//! Project version string for DynamicLink.
+FOUNDATION_EXPORT const unsigned char DynamicLinkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DynamicLink/PublicHeader.h>
+
+
